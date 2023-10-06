577a31c039073b5d1f33e8cfbf6e390c7849e622
TantoFaz <thiago.playgames15@gmail.com>
2023-10-06 16:17:41 -0300

    inital commit

---
+++ src/components/header/index.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react"
+
+import "./style.scss"
+
+const Header: FC = () => {
+    return (
+        <header className="header">
+            <h1>Header</h1>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react"
+
+import "./style.scss"
+
+const HomePage: FC = () => {
+    return (
+        <>
+            <h1>Home Page</h1>
+        </>
+    )
+}
+
+export default HomePage
